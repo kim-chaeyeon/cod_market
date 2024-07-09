64e3292ca5f68004738b1cabef6c6df3839d291b
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-07-09 14:12:57 +0900

    login & join controller

--- src/main/java/com/cod/market/member/controller/MemberController.java
+++ src/main/java/com/cod/market/member/controller/MemberController.java
@@ -1,7 +1,16 @@
 package com.cod.market.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/member")
 public class MemberController {
+    @GetMapping("/login")
+    @ResponseBody
+    public String join() {
+        return "회원가입 페이지 입니다.";
+    }
 }
